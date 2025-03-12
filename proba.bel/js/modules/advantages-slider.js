231499b91bdfcb5d558c16364f8b6bb8c56fc52d
djhjyjd46 <101903235+djhjyjd46@users.noreply.github.com>
2025-03-12 10:59:43 +0300

    слайдер 2

---
+++ proba.bel/js/modules/advantages-slider.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const advantagesSwiper = new Swiper('.advantages .swiper', {
+        slidesPerView: 4,
+        spaceBetween: 24,
+        loop: true,
+        navigation: {
+            nextEl: '.advantages__right',
+            prevEl: '.advantages__left',
+        },
+        breakpoints: {
+            // когда ширина экрана >= 576px
+            576: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // когда ширина экрана >= 768px
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            // когда ширина экрана >= 992px
+            992: {
+                slidesPerView: 4,
+                spaceBetween: 30
+            }
+        }
+    });
+});
