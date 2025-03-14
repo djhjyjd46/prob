0505c43e7b3841c80d0da896c946b37ccf4d6218
djhjyjd46 <101903235+djhjyjd46@users.noreply.github.com>
2025-03-14 08:32:09 +0300

    22

--- proba.bel/js/modules/hero-slider.js
+++ proba.bel/js/modules/hero-slider.js
@@ -4,7 +4,7 @@ const heroSwiper = new Swiper('.hero-swiper', {
     spaceBetween: 30,
     loop: true,
     autoplay: {
-        delay: 400000,
+        delay: 4000,
         disableOnInteraction: false,
     },
     effect: 'fade',
@@ -17,7 +17,7 @@ const heroSwiper = new Swiper('.hero-swiper', {
         el: '.hero-pagination',
         clickable: true,
         renderBullet: function (index, className) {
-            // Кастомная пагинация с SVG (светлые кружки по умолчанию, активный - темный)
+            // Кастомная пагинация с SVG
             return `<div class="${className}"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="10" cy="10" r="10" fill="#F7FAF9" opacity="1" />
         </svg></div>`;
@@ -33,14 +33,16 @@ heroSwiper.on('slideChange', function () {
     // Перебираем их и меняем цвет
     bullets.forEach((bullet, idx) => {
         const circle = bullet.querySelector('circle');
-        if (bullet.classList.contains('swiper-pagination-bullet-active')) {
-            // Активный буллет - темный
-            circle.setAttribute('fill', '#599D86');
-            circle.setAttribute('opacity', '1');
-        } else {
-            // Неактивные буллеты - светлые
-            circle.setAttribute('fill', '#599D86');
-            circle.setAttribute('opacity', '0.4');
+        if (circle) { // Добавляем проверку на существование элемента
+            if (bullet.classList.contains('swiper-pagination-bullet-active')) {
+                // Активный буллет - темный
+                circle.setAttribute('fill', '#599D86');
+                circle.setAttribute('opacity', '1');
+            } else {
+                // Неактивные буллеты - светлые
+                circle.setAttribute('fill', '#599D86');
+                circle.setAttribute('opacity', '0.4');
+            }
         }
     });
 });
